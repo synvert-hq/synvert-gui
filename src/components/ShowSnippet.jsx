4b52eeab24835f7147ea6af149a52e53410aedcb
Richard Huang <flyerhzm@gmail.com>
2021-02-19 09:11:17 +0800

    highlight code section

--- src/components/ShowSnippet.jsx
+++ src/components/ShowSnippet.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactMarkdown from 'react-markdown'
 import AppContext from '../context'
+import Prism from 'prismjs';
 
-export default () => (
-    <AppContext.Consumer>
-        {({ currentSnippet }) => (
-            <div>{currentSnippet ? <ReactMarkdown children={currentSnippet.description} /> : ''}</div>
-        )}
-    </AppContext.Consumer>
-)
+export default () => {
+    useEffect(() => {
+        Prism.highlightAll();
+    })
+
+    return (
+        <AppContext.Consumer>
+            {({ currentSnippet }) => (
+                <div>{currentSnippet ? <ReactMarkdown children={currentSnippet.description} /> : ''}</div>
+            )}
+        </AppContext.Consumer>
+    )
+}
--- src/components/SnippetHeader.jsx
+++ src/components/SnippetHeader.jsx
@@ -6,7 +6,7 @@ export default () => (
         {({ path, currentSnippet, setPath, runSnippet }) => (
             <div className="d-flex align-items-center mb-4">
                 <span>Workspace:</span>
-                <div class="input-group input-group-lg ml-2 mr-2">
+                <div className="input-group input-group-lg ml-2 mr-2">
                     <input className="flex-grow-1 form-control" type="text" value={path} placeholder="directory path" onChange={(e) => setPath(e.target.value)} />
                 </div>
                 <button className="btn btn-primary" disabled={!path || !currentSnippet} onClick={runSnippet}>Run</button>
