8f2fab74799ac73380c2f7d5bdbdd9abd04d5887
Richard Huang <flyerhzm@gmail.com>
2021-02-19 14:17:33 +0800

    show snippet sub snippets

--- src/components/ShowSnippet.jsx
+++ src/components/ShowSnippet.jsx
@@ -17,6 +17,17 @@ export default () => {
                     <>
                         <h2>{snippet.group}/{snippet.name}</h2>
                         <div><ReactMarkdown children={snippet.description} /></div>
+                        <ul>
+                            {snippet.sub_snippets.map(subSnippetName => {
+                                const subSnippet = snippetsStore[`${snippet.group}/${subSnippetName}`]
+                                return (
+                                    <li>
+                                        <h4>{subSnippet.name}</h4>
+                                        <div><ReactMarkdown children={subSnippet.description} /></div>
+                                    </li>
+                                )
+                            })}
+                        </ul>
                     </>
                 )
             }}
