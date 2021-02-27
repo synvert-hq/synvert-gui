c12eb4e7b6c06ef8e3a68a944ec8c5bc2d4172f3
Richard Huang <flyerhzm@gmail.com>
2021-02-27 14:11:50 +0800

    add forge.config.js

---
+++ forge.config.js
@@ -0,0 +1,82 @@
+require('dotenv').config()
+
+const config = {
+  packagerConfig: {
+    "osxSign": {
+      "identity": "Developer ID Application: Zhimin Huang (9S5K3LWH74)",
+      "hardened-runtime": true,
+      "gatekeeper-assess": false,
+      "entitlements": "entitlements.plist",
+      "entitlements-inherit": "entitlements.plist",
+      "signature-flags": "library"
+    }
+  },
+  "makers": [
+    {
+      "name": "@electron-forge/maker-squirrel",
+      "config": {
+        "name": "snippets"
+      }
+    },
+    {
+      "name": "@electron-forge/maker-zip",
+      "platforms": [
+        "darwin"
+      ]
+    },
+    {
+      "name": "@electron-forge/maker-deb",
+      "config": {}
+    },
+    {
+      "name": "@electron-forge/maker-rpm",
+      "config": {}
+    }
+  ],
+  "plugins": [
+    [
+      "@electron-forge/plugin-webpack",
+      {
+        "mainConfig": "./webpack.main.config.js",
+        "renderer": {
+          "config": "./webpack.renderer.config.js",
+          "entryPoints": [
+            {
+              "html": "./src/index.html",
+              "js": "./src/renderer.js",
+              "name": "main_window"
+            }
+          ]
+        }
+      }
+    ]
+  ]
+}
+
+function notarizeMaybe() {
+  if (process.platform !== 'darwin') {
+    return;
+  }
+
+  if (!process.env.CI) {
+    console.log(`Not in CI, skipping notarization`);
+    return;
+  }
+
+  if (!process.env.APPLE_ID || !process.env.APPLE_ID_PASSWORD) {
+    console.warn(
+      'Should be notarizing, but environment variables APPLE_ID or APPLE_ID_PASSWORD are missing!',
+    );
+    return;
+  }
+
+  config.packagerConfig.osxNotarize = {
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_ID_PASSWORD
+  };
+}
+
+notarizeMaybe();
+
+// Finally, export it
+module.exports = config;
