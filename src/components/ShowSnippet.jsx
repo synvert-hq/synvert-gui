4a74d3d49c0583f40f6eb5455ea5df3d3c03f7db
Richard Huang <flyerhzm@gmail.com>
2021-03-07 14:15:07 +0800

    add modal-backdrop

--- src/components/ShowSnippet.jsx
+++ src/components/ShowSnippet.jsx
@@ -69,26 +69,29 @@ export default () => {
                 <button className="btn btn-primary float-right" onClick={show}>Show Source Code</button>
             </div>
             {showCode && (
-                <div className="modal fade show" data-backdrop="static" style={{ display: 'block' }}>
-                    <div className="modal-dialog modal-lg modal-dialog-scrollable">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h5 className="modal-title">{snippet.group}/{snippet.name}</h5>
-                                <button type="button" className="close" onClick={close}>
-                                    <span>&times;</span>
-                                </button>
-                            </div>
-                            <div className="modal-body">
-                                {code === '' ? 'Loading...' : (
-                                    <pre className="language-ruby"><code className="language-ruby">{code}</code></pre>
-                                )}
-                            </div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-secondary" onClick={close}>Close</button>
+                <>
+                    <div className="modal fade show" data-backdrop="static" style={{ display: 'block' }}>
+                        <div className="modal-dialog modal-lg modal-dialog-scrollable">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h5 className="modal-title">{snippet.group}/{snippet.name}</h5>
+                                    <button type="button" className="close" onClick={close}>
+                                        <span>&times;</span>
+                                    </button>
+                                </div>
+                                <div className="modal-body">
+                                    {code === '' ? 'Loading...' : (
+                                        <pre className="language-ruby"><code className="language-ruby">{code}</code></pre>
+                                    )}
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-secondary" onClick={close}>Close</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                    <div className="modal-backdrop fade show"></div>
+                </>
             )}
         </LoadingOverlay>
     )
