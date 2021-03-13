3386b7845057305988018880759f0e31701e196f
Richard Huang <flyerhzm@gmail.com>
2021-03-13 21:34:16 +0800

    remove bootstrap-icons

--- src/components/SnippetHeader.jsx
+++ src/components/SnippetHeader.jsx
@@ -12,7 +12,7 @@ export default ({ selectPath }) => (
                     <input type="text" className="form-control" placeholder="directory path" value={path} readOnly onClick={selectPath} />
                     <div className="input-group-append">
                         <button className="btn btn-outline-secondary" type="button" onClick={selectPath}>
-                            <i className="bi-three-dots"></i>
+                            ...
                         </button>
                     </div>
                 </div>
