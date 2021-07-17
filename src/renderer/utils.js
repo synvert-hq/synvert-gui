aafab2a53bec382de580f0499fefac23ffb0dcd0
Richard Huang <flyerhzm@gmail.com>
2021-07-17 08:35:50 +0800

    refactor get and save preference

--- src/renderer/utils.js
+++ src/renderer/utils.js
@@ -1,66 +1,41 @@
 import { ipcRenderer } from 'electron';
 
+const savePreference = (key, value) => {
+    const preferences = ipcRenderer.sendSync('getPreferences')
+    if (!preferences.settings) {
+        preferences.settings = {}
+    }
+    preferences.settings[key] = value
+    ipcRenderer.sendSync('setPreferences', preferences)
+}
+
+const getPreference = (key) => {
+    const preferences = ipcRenderer.sendSync('getPreferences')
+    return !!(preferences && preferences.settings && preferences.settings[key])
+}
+
+const PREFERENCE_DEPENDENCY = 'dependency'
 export const DOCKER_DEPENDENCY = 'docker'
 export const NATIVE_DEPENDENCY = 'native'
 
-export const selectDockerDependency = () => {
-    const preferences = ipcRenderer.sendSync('getPreferences')
-    preferences.settings.dependency = DOCKER_DEPENDENCY
-    ipcRenderer.sendSync('setPreferences', preferences)
-}
-export const selectNativeDependency = () => {
-    const preferences = ipcRenderer.sendSync('getPreferences')
-    preferences.settings.dependency = NATIVE_DEPENDENCY
-    ipcRenderer.sendSync('setPreferences', preferences)
-}
-export const dependencySelected = () => {
-    const preferences = ipcRenderer.sendSync('getPreferences')
-    return !!(preferences && preferences.settings && preferences.settings.dependency)
-}
-export const dockerDependencySelected = () => {
-    const preferences = ipcRenderer.sendSync('getPreferences')
-    return preferences.settings.dependency === DOCKER_DEPENDENCY
-}
-export const nativeDependencySelected = () => {
-    const preferences = ipcRenderer.sendSync('getPreferences')
-    return preferences.settings.dependency === NATIVE_DEPENDENCY
-}
+export const selectDockerDependency = () => savePreference(PREFERENCE_DEPENDENCY, DOCKER_DEPENDENCY)
+export const selectNativeDependency = () => savePreference(PREFERENCE_DEPENDENCY, NATIVE_DEPENDENCY)
+export const dependencySelected = () => !!getPreference(PREFERENCE_DEPENDENCY)
+export const dockerDependencySelected = () => getPreference(PREFERENCE_DEPENDENCY) === DOCKER_DEPENDENCY
+export const nativeDependencySelected = () => getPreference(PREFERENCE_DEPENDENCY) === NATIVE_DEPENDENCY
 
+const PREFERENCE_SHOW_DIFFS = 'show_diffs'
 export const SHOW_DIFFS_ALWAYS_SHOW = 'always_show'
 export const SHOW_DIFFS_NEVER_SHOW = 'never_show'
 export const SHOW_DIFFS_ASK_ME = 'ask_me'
 
-export const selectShowDiffsAlwaysShow = () => {
-    const preferences = ipcRenderer.sendSync('getPreferences')
-    preferences.settings.show_diffs = SHOW_DIFFS_ALWAYS_SHOW
-    ipcRenderer.sendSync('setPreferences', preferences)
-}
-export const selectShowDiffsNeverShow = () => {
-    const preferences = ipcRenderer.sendSync('getPreferences')
-    preferences.settings.show_diffs = SHOW_DIFFS_NEVER_SHOW
-    ipcRenderer.sendSync('setPreferences', preferences)
-}
-export const selectShowDiffsAskMe = () => {
-    const preferences = ipcRenderer.sendSync('getPreferences')
-    preferences.settings.show_diffs = SHOW_DIFFS_ASK_ME
-    ipcRenderer.sendSync('setPreferences', { settings: { show_diffs: SHOW_DIFFS_ASK_ME } })
-}
-export const showDiffsSelected = () => {
-    const preferences = ipcRenderer.sendSync('getPreferences')
-    return !!(preferences && preferences.settings && preferences.settings.show_diffs)
-}
-export const showDiffsAlwaysShowSelected = () => {
-    const preferences = ipcRenderer.sendSync('getPreferences')
-    return preferences.settings.show_diffs === SHOW_DIFFS_ALWAYS_SHOW
-}
-export const showDiffsNeverShowSelected = () => {
-    const preferences = ipcRenderer.sendSync('getPreferences')
-    return preferences.settings.show_diffs === SHOW_DIFFS_NEVER_SHOW
-}
-export const showDiffsAskMeSelected = () => {
-    const preferences = ipcRenderer.sendSync('getPreferences')
-    return preferences.settings.show_diffs === SHOW_DIFFS_ASK_ME
-}
+export const selectShowDiffsAlwaysShow = () => savePreference(PREFERENCE_SHOW_DIFFS, SHOW_DIFFS_ALWAYS_SHOW)
+export const selectShowDiffsNeverShow = () => savePreference(PREFERENCE_SHOW_DIFFS, SHOW_DIFFS_NEVER_SHOW)
+export const selectShowDiffsAskMe = () => savePreference(PREFERENCE_SHOW_DIFFS, SHOW_DIFFS_ASK_ME)
+export const showDiffsSelected = () => !!getPreference(PREFERENCE_SHOW_DIFFS)
+export const showDiffsAlwaysShowSelected = () => getPreference(PREFERENCE_SHOW_DIFFS, SHOW_DDIFFS_ALWAYS_SHOW)
+export const showDiffsNeverShowSelected = () => getPreference(PREFERENCE_SHOW_DIFFS, SHOW_DIFFS_NEVER_SHOW)
+export const showDiffsAskMeSelected = () => getPreference(PREFERENCE_SHOW_DIFFS, SHOW_DIFFS_ASK_ME)
 
 export const convertSnippetsToStore = (snippets) =>
     snippets.reduce(
