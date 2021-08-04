bcb737b62465a4402544e8479e4728d34a9c5dce
Richard Huang <flyerhzm@gmail.com>
2021-08-04 21:05:51 +0800

    update SelectDependencies text

--- src/renderer/components/SelectDependencies.jsx
+++ src/renderer/components/SelectDependencies.jsx
@@ -23,9 +23,10 @@ export default ({ setDependency }) => {
           <div className="card">
             <div className="card-body">
               <h5 className="card-title">Docker (recommended)</h5>
-              <p className="card-text">
-                It will use docker image for all dependencies.
-              </p>
+              <div className="card-text">
+                <p>It will use docker image for all dependencies.</p>
+                <p>Make sure you can connect docker daemon.</p>
+              </div>
               <div className="text-right">
                 <button onClick={selectDocker} className="btn btn-primary">
                   Select
@@ -38,9 +39,10 @@ export default ({ setDependency }) => {
           <div className="card">
             <div className="card-body">
               <h5 className="card-title">Native</h5>
-              <p className="card-text">
-                It will your system ruby and synvert gem.
-              </p>
+              <div className="card-text">
+                <p>It will your system ruby and synvert gem.</p>
+                <p>Make sure you have permission to install synvert gem.</p>
+              </div>
               <div className="text-right">
                 <button onClick={selectNative} className="btn btn-primary">
                   Select
@@ -49,6 +51,9 @@ export default ({ setDependency }) => {
             </div>
           </div>
         </div>
+      </div>
+      <div className="text-center mt-5">
+        Selection can be changed from menu Preferences later.
       </div>
     </div>
   );
