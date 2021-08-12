11786fe69a53e430c49c7d351dbf4e74f0194f51
Richard Huang <flyerhzm@gmail.com>
2021-08-12 19:17:33 +0800

    app.once for ready event

--- src/main/main.js
+++ src/main/main.js
@@ -15,7 +15,7 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', () => {
+app.once('ready', () => {
   getOrCreateMainWindow();
   setupAboutPanel();
   setupMenu();
