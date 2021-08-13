f283a33a9863b43b2f40497f735f21cbb566c383
Richard Huang <flyerhzm@gmail.com>
2021-08-13 20:59:19 +0800

    try to fix autoUpdater in windows

--- src/main/update.js
+++ src/main/update.js
@@ -13,22 +13,29 @@ export const setupUpdates = () => {
   }
 
   autoUpdater.setFeedURL(updaterFeedURL);
-  /* Log whats happening
-  TODO send autoUpdater events to renderer so that we could console log it in developer tools
-  You could alsoe use nslog or other logging to see what's happening */
-  autoUpdater.on('error', err => console.log(err));
-  autoUpdater.on('checking-for-update', () => console.log('checking-for-update'));
-  autoUpdater.on('update-available', () => console.log('update-available'));
-  autoUpdater.on('update-not-available', () => console.log('update-not-available'));
+
+  autoUpdater.on('checking-for-update', () => {
+    console.log('The autoUpdater is checking for an update')
+  });
+  autoUpdater.on('update-available', () => {
+    console.log('The autoUpdater has found an update and is now downloading it!')
+  });
+  autoUpdater.on('update-not-available', () => {
+    console.log('The autoUpdater has not found any updates :(')
+  });
 
   // Ask the user if update is available
-  autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+  autoUpdater.on('update-downloaded', (event, notes, name, date) => {
+    console.log('The autoUpdater has downloaded an update!')
+    console.log(`The new release is named ${name} and was released on ${date}`)
+    console.log(`The release notes are: ${notes}`)
+
     // Ask user to update the app
     dialog.showMessageBox({
       type: 'info',
       buttons: ['Restart', 'Later'],
       title: 'Application Update',
-      message: process.platform === 'win32' ? releaseNotes : releaseName,
+      message: name,
       detail: 'A new version has been downloaded. Restart the application to apply the updates.'
     }).then(({ response }) => {
       if (response === 0) {
