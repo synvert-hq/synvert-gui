0ec0c5d03c653863dd04c9e0c10a150bc92e4489
Richard Huang <flyerhzm@gmail.com>
2022-12-02 13:15:28 +0800

    quit app when all window closed in mac

--- src/main/main.js
+++ src/main/main.js
@@ -42,7 +42,5 @@ app.whenReady().then(() => {
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+  app.quit();
 });
